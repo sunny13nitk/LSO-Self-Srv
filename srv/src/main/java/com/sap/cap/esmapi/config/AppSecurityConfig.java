8c8fa121691afbc916a654563579d04ff45eeb4b
sunny13nitk <sunny13nitk@gmail.com>
2025-03-18 13:22:39 +0530

    index page changes

--- srv/src/main/java/com/sap/cap/esmapi/config/AppSecurityConfig.java
+++ srv/src/main/java/com/sap/cap/esmapi/config/AppSecurityConfig.java
@@ -83,6 +83,7 @@ public class AppSecurityConfig
                            authz
                                 .requestMatchers("/login/**").permitAll()
                                 .requestMatchers("/static/**").permitAll()
+                                .requestMatchers("/isr/**").permitAll()
                                 .requestMatchers("/web-component.js/**").permitAll()
                                 .requestMatchers("/lso/**").hasAnyAuthority(GC_Constants.gc_role_employee_lso, GC_Constants.gc_role_contractor_lso)
                                 .requestMatchers("/post/**").hasAnyAuthority(GC_Constants.gc_role_employee_lso, GC_Constants.gc_role_contractor_lso)
