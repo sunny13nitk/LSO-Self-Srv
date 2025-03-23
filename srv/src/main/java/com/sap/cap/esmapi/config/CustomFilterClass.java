cb1b3e7e4273b03c2f3731068f6c729cd04e9c82
sunny13nitk <sunny13nitk@gmail.com>
2025-03-23 15:03:40 +0530

    log filter error

--- srv/src/main/java/com/sap/cap/esmapi/config/CustomFilterClass.java
+++ srv/src/main/java/com/sap/cap/esmapi/config/CustomFilterClass.java
@@ -26,6 +26,9 @@ public class CustomFilterClass extends OncePerRequestFilter
         }
         catch (Exception e)
         {
+            log.error(
+                    "Invalid Token! Access to app not possible. Try clearing browser history and cookies and reaccessing the app. You can also try logging in via a private/Incognito window. "
+                            + e.getLocalizedMessage());
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
                     "Invalid Token! Access to app not possible. Try clearing browser history and cookies and reaccessing the app. You can also try logging in via a private/Incognito window.");
         }
