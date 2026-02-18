bb54b83a338a4dfdeaa58892c4fc6da40c25a8e2
sunny13nitk <sunny13nitk@gmail.com>
2026-02-18 12:19:04 +0530

    catg2 for submit

--- srv/src/main/java/com/sap/cap/esmapi/catg/srv/intf/IF_CatalogSrv.java
+++ srv/src/main/java/com/sap/cap/esmapi/catg/srv/intf/IF_CatalogSrv.java
@@ -14,6 +14,8 @@ public interface IF_CatalogSrv
 {
     public TY_CatalogTree getCaseCatgTree4LoB(EnumCaseTypes caseType) throws EX_ESMAPI;
 
+    // Get Complete Category Hierarchy passing most granular Category Id and Case
+    // Type
     public String[] getCatgHierarchyforCatId(String catId, EnumCaseTypes caseType) throws EX_ESMAPI;
 
     public TY_CatgTemplates getTemplates4Catg(String catId, EnumCaseTypes caseType) throws EX_ESMAPI;
