bd9b65025441c4994878f4373d1ed7bf3bc38c13
C5411693 <panyam.gangadhar.reddy@sap.com>
2026-08-26 10:22:42 +0530

    Modified the condition check for logging the country

--- srv/src/main/java/com/sap/cap/esmapi/hana/logging/srv/impl/CL_HANALoggingSrv.java
+++ srv/src/main/java/com/sap/cap/esmapi/hana/logging/srv/impl/CL_HANALoggingSrv.java
@@ -22,6 +22,7 @@ import org.springframework.util.StringUtils;
 import com.sap.cap.esmapi.exceptions.EX_ESMAPI;
 import com.sap.cap.esmapi.hana.logging.srv.intf.IF_HANALoggingSrv;
 import com.sap.cap.esmapi.utilities.constants.GC_Constants;
+import com.sap.cap.esmapi.utilities.enums.EnumMessageType;
 import com.sap.cap.esmapi.utilities.pojos.TY_Message;
 import com.sap.cds.Result;
 import com.sap.cds.ql.Insert;
@@ -78,8 +79,8 @@ public class CL_HANALoggingSrv implements IF_HANALoggingSrv
                 logEntity.put("objectid", logMsg.getObjectId()); // Object ID
 
                 String baseMsg = logMsg.getMessage();
-                if (StringUtils.hasText(logMsg.getCountry()) || StringUtils.hasText(logMsg.getCategory1())
-                        || StringUtils.hasText(logMsg.getCategory2()))
+                if (EnumMessageType.SUCC_CASE_SUBM.equals(logMsg.getMsgType())
+                        || EnumMessageType.SUCC_CASE_CREATE.equals(logMsg.getMsgType()))
                 {
                     ObjectMapper objectMapper = new ObjectMapper();
                     ObjectNode meta = objectMapper.createObjectNode();
